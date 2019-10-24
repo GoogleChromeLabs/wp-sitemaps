28924382509417a5d7a904c94eb34ce91d0f6abe
Sander van Dragt <sander@humanmade.com>
2019-10-24 10:56:31 +0100

    Register core_sitemaps_page custom post type.

--- core-sitemaps.php
+++ core-sitemaps.php
@@ -14,12 +14,33 @@
 
 defined( 'ABSPATH' ) or die();
 
-// require_once __DIR__ . '/inc/class-core-sitemaps-page.php';
+const CORE_SITEMAPS_TEXT_DOMAIN = 'core-sitemaps';
+const CORE_SITEMAPS_CPT_PAGE    = 'core_sitemaps_page';
+
+require_once __DIR__ . '/inc/class-core-sitemaps-page.php';
 
 /**
  * Bootstrapping.
  */
 function core_sitemaps_init() {
+	core_sitemaps_register_ctp();
 }
 
 add_action( 'init', 'core_sitemaps_init', 10 );
+
+function core_sitemaps_register_ctp() {
+	$labels = array(
+		'name'          => _x( 'Sitemap Pages', 'Sitemap Page General Name', CORE_SITEMAPS_TEXT_DOMAIN ),
+		'singular_name' => _x( 'Sitemap Page', 'Sitemap Page Singular Name', CORE_SITEMAPS_TEXT_DOMAIN ),
+	);
+	$args   = array(
+		'label'           => __( 'Sitemap Page', CORE_SITEMAPS_TEXT_DOMAIN ),
+		'description'     => __( 'Bucket of sitemap links', CORE_SITEMAPS_TEXT_DOMAIN ),
+		'labels'          => $labels,
+		'supports'        => array( 'editor', 'custom-fields' ),
+		'can_export'      => false,
+		'rewrite'         => false,
+		'capability_type' => 'post',
+	);
+	register_post_type( CORE_SITEMAPS_CPT_PAGE, $args );
+}
