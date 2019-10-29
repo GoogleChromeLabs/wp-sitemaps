47257ef9a6e769704107ff218a3e9016e23967c9
Sander van Dragt <sander@humanmade.com>
2019-10-29 17:40:30 +0000

    Switch to skeleton bootstrap.

--- core-sitemaps.php
+++ core-sitemaps.php
@@ -21,22 +21,5 @@ require_once __DIR__ . '/inc/class-sitemaps-index.php';
 require_once __DIR__ . '/inc/class-sitemaps-posts.php';
 require_once __DIR__ . '/inc/class-sitemaps-registry.php';
 
-/**
- *
- * A helper function to initiate actions, hooks and other features needed.
- *
- * @uses add_action()
- * @uses add_filter()
- */
-function core_sitemaps_bootstrap() {
-	$core_sitemaps_index = new Core_Sitemaps_Index();
-	add_action( 'init', array( $core_sitemaps_index, 'url_rewrites' ), 99 );
-	add_filter( 'redirect_canonical', array( $core_sitemaps_index, 'redirect_canonical' ) );
-	add_filter( 'template_include', array( $core_sitemaps_index, 'output_sitemap' ) );
-
-	$core_sitemaps_posts = new Core_Sitemaps_Posts();
-	add_action( 'init', array( $core_sitemaps_posts, 'url_rewrites' ), 99 );
-	add_filter( 'template_include', array( $core_sitemaps_posts, 'template' ) );
-}
-
-add_filter( 'init', 'core_sitemaps_bootstrap' );
+$core_sitemaps_index = new Core_Sitemaps_Index();
+$core_sitemaps_index->bootstrap();
