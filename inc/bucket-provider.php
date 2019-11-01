c4af01ce882fce6532558d67932d6db8a948e5f4
Sander van Dragt <sander@humanmade.com>
2019-11-01 12:38:35 +0000

    Bucket basics

--- inc/bucket-provider.php
+++ inc/bucket-provider.php
@@ -19,3 +19,22 @@ function core_sitemaps_bucket_register() {
 	);
 	register_post_type( CORE_SITEMAPS_CPT_BUCKET, $args );
 }
+
+function core_sitemaps_bucket_create( $post ) {
+	$args = array(
+		'post_type'    => CORE_SITEMAPS_CPT_BUCKET,
+		'post_content' => wp_json_encode(
+			array(
+				// TODO implemenet data
+				$post->ID => 'core_sitemaps_url_content( $post )',
+			)
+		),
+		'meta_input'   => array(
+			'bucket_num' => $bucket_num,
+			'post_type'  => $post->post_type,
+		),
+		'post_status'  => 'publish',
+	);
+
+	return wp_insert_post( $args );
+}
