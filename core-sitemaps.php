6c07b980f0b949d856eb5bf0cf7699b4eddaeda4
Sander van Dragt <sander@humanmade.com>
2019-11-08 16:16:55 +0000

    Delete pages.

--- core-sitemaps.php
+++ core-sitemaps.php
@@ -25,7 +25,6 @@ const CORE_SITEMAPS_MAX_URLS       = 50000;
 require_once __DIR__ . '/inc/class-sitemaps.php';
 require_once __DIR__ . '/inc/class-sitemaps-provider.php';
 require_once __DIR__ . '/inc/class-sitemaps-index.php';
-require_once __DIR__ . '/inc/class-sitemaps-pages.php';
 require_once __DIR__ . '/inc/class-sitemaps-post-types.php';
 require_once __DIR__ . '/inc/class-sitemaps-registry.php';
 require_once __DIR__ . '/inc/class-sitemaps-renderer.php';
