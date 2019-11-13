257cd5c4c57fbf49c9c4f724323cc6616e7409bc
Sander van Dragt <sander@humanmade.com>
2019-11-13 09:18:23 +0000

    phpcs

--- inc/class-core-sitemaps.php
+++ inc/class-core-sitemaps.php
@@ -61,7 +61,6 @@ class Core_Sitemaps {
 		 * @since 0.1.0
 		 *
 		 * @param array $providers Array of Core_Sitemap_Provider objects.
-		 *
 		 */
 		$providers = apply_filters(
 			'core_sitemaps_register_providers',
