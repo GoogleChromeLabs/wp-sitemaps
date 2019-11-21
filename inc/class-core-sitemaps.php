671a087549a1f5e80275b5d1c0a2c6156763348e
Kirsty Burgoine <kirsty@humanmade.com>
2019-11-21 19:10:47 +0000

    67: Combine rewrite functions into one

--- inc/class-core-sitemaps.php
+++ inc/class-core-sitemaps.php
@@ -42,8 +42,7 @@ class Core_Sitemaps {
 		add_action( 'init', array( $this, 'setup_sitemaps_index' ) );
 		add_action( 'init', array( $this, 'register_sitemaps' ) );
 		add_action( 'init', array( $this, 'setup_sitemaps' ) );
-		add_action( 'init', array( $this, 'xsl_stylesheet_rewrite' ) );
-		add_action( 'init', array( $this, 'index_xsl_stylesheet_rewrite' ) );
+		add_action( 'init', array( $this, 'xsl_stylesheet_rewrites' ) );
 		add_action( 'wp_loaded', array( $this, 'maybe_flush_rewrites' ) );
 	}
 
@@ -101,18 +100,9 @@ class Core_Sitemaps {
 	/**
 	 * Provide rewrite for the xsl stylesheet.
 	 */
-	public function xsl_stylesheet_rewrite() {
+	public function xsl_stylesheet_rewrites() {
 		add_rewrite_tag( '%stylesheet%', '([^?]+)' );
 		add_rewrite_rule( '^sitemap\.xsl$', 'index.php?stylesheet=xsl', 'top' );
-
-		$stylesheet = new Core_Sitemaps_Stylesheet();
-		add_action( 'template_redirect', array( $stylesheet, 'render_stylesheet' ) );
-	}
-
-	/**
-	 * Provide rewrite for the sitemap index xsl stylesheet.
-	 */
-	public function index_xsl_stylesheet_rewrite() {
 		add_rewrite_rule( '^sitemap-index\.xsl$', 'index.php?stylesheet=index', 'top' );
 
 		$stylesheet = new Core_Sitemaps_Stylesheet();
