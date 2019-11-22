91eb9628a7b19b134909fc6e0970804c35373d5c
Kirsty Burgoine <kirsty@humanmade.com>
2019-11-22 12:48:32 +0000

    80: Add homepage to sitemap index if posts page

--- inc/class-core-sitemaps-renderer.php
+++ inc/class-core-sitemaps-renderer.php
@@ -96,11 +96,18 @@ class Core_Sitemaps_Renderer {
 		header( 'Content-type: application/xml; charset=UTF-8' );
 		$sitemap_index = new SimpleXMLElement( '<?xml version="1.0" encoding="UTF-8" ?>' . $this->stylesheet_index . '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></sitemapindex>' );
 
+		if ( 'posts' == get_option('show_on_front') ) {
+			$sitemap = $sitemap_index->addChild( 'sitemap' );
+			$sitemap->addChild( 'loc', home_url() );
+			$sitemap->addChild( 'lastmod', '2004-10-01T18:23:17+00:00' );
+		}
+
 		foreach ( $sitemaps as $slug ) {
 			$sitemap = $sitemap_index->addChild( 'sitemap' );
 			$sitemap->addChild( 'loc', esc_url( $this->get_sitemap_url( $slug ) ) );
 			$sitemap->addChild( 'lastmod', '2004-10-01T18:23:17+00:00' );
 		}
+
 		// All output is escaped within the addChild method calls.
 		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		echo $sitemap_index->asXML();
