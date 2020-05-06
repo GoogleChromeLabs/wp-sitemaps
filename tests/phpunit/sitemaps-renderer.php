965d295629fdec2ca53be19c109861d9a355e2ef
Felix Arntz <felixarntz@google.com>
2020-05-06 08:16:47 -0700

    Update tests to reflect query var value change.

--- tests/phpunit/sitemaps-renderer.php
+++ tests/phpunit/sitemaps-renderer.php
@@ -8,7 +8,7 @@ class Test_Core_Sitemaps_Renderer extends WP_UnitTestCase {
 		$sitemap_renderer = new Core_Sitemaps_Renderer();
 		$stylesheet_url   = $sitemap_renderer->get_sitemap_stylesheet_url();
 
-		$this->assertStringEndsWith( '/?sitemap-stylesheet=xsl', $stylesheet_url );
+		$this->assertStringEndsWith( '/?sitemap-stylesheet=sitemap', $stylesheet_url );
 	}
 
 	public function test_get_sitemap_stylesheet_url_pretty_permalinks() {
@@ -132,7 +132,7 @@ class Test_Core_Sitemaps_Renderer extends WP_UnitTestCase {
 
 		$actual   = $renderer->get_sitemap_xml( $url_list );
 		$expected = '<?xml version="1.0" encoding="UTF-8"?>' .
-					'<?xml-stylesheet type="text/xsl" href="http://' . WP_TESTS_DOMAIN . '/?sitemap-stylesheet=xsl" ?>' .
+					'<?xml-stylesheet type="text/xsl" href="http://' . WP_TESTS_DOMAIN . '/?sitemap-stylesheet=sitemap" ?>' .
 					'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' .
 					'<url><loc>http://' . WP_TESTS_DOMAIN . '/2019/10/post-1</loc></url>' .
 					'<url><loc>http://' . WP_TESTS_DOMAIN . '/2019/10/post-2</loc></url>' .
